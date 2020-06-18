0d9ae0a5f835fc153a31b5f9b5ba6b3a2cd2eaab
rijalwafi <bekasi>
2020-06-18 21:46:16 +0700

    change some front end

---
+++ latihan/latihanFungsi.js
@@ -0,0 +1,12 @@
+const tulisNama = (nama, umur) => {
+  if (!nama === "rijal") {
+    console.log( "Good Morning");
+  }if else (!umur===24){
+    return "Selamat pagi";
+  }else{
+
+  }else{
+      
+  }
+};
+console.log(tulisNama("rijal"));
